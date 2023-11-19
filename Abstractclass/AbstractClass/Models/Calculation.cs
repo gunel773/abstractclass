a00bebcf94c4b49c6debc1ce2e7c37523d066895
GUNEL <146637952+gunel773@users.noreply.github.com>
2023-11-19 09:25:28 +0000

    to correct a mistake

--- Abstractclass/AbstractClass/Models/Calculation.cs
+++ Abstractclass/AbstractClass/Models/Calculation.cs
@@ -1,11 +1,4 @@
-﻿using AbstractClass.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AbstractClass.Models
+﻿namespace AbstractClass.Models
 {
     public class Calculation:ICalculation
     {
